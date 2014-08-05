8198fafce0d6ddbfbae392548bfc18d017aaa7ad
Kelly Norton <kellegous@gmail.com>
2014-08-05 07:05:53 -0400

    Fetch github user info.

--- src/four04/auth/auth.go
+++ src/four04/auth/auth.go
@@ -2,14 +2,26 @@ package auth
 
 import (
   "code.google.com/p/goauth2/oauth"
+  "encoding/json"
   "fmt"
   "github.com/kellegous/pork"
   "net/http"
+  "time"
 )
 
 type Context interface {
   ClientId() string
   ClientSecret() string
+}
+
+type ghUser struct {
+  Email     string    `json:"email"`
+  Name      string    `json:"name"`
+  Company   string    `json:"company"`
+  Location  string    `json:"location"`
+  Blog      string    `json:"blog"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
 }
 
 func urlFor(r *http.Request) string {
@@ -31,6 +43,20 @@ func configFromRequest(ctx Context, r *http.Request) *oauth.Config {
   }
 }
 
+func fetchUser(tx *oauth.Transport, user *ghUser) error {
+  c := http.Client{
+    Transport: tx,
+  }
+
+  res, err := c.Get("https://api.github.com/user")
+  if err != nil {
+    return err
+  }
+  defer res.Body.Close()
+
+  return json.NewDecoder(res.Body).Decode(user)
+}
+
 func Setup(r pork.Router, ctx Context) {
   r.RespondWithFunc("/auth/a", func(w pork.ResponseWriter, r *http.Request) {
     http.Redirect(w, r,
@@ -39,5 +65,28 @@ func Setup(r pork.Router, ctx Context) {
   })
 
   r.RespondWithFunc("/auth/z", func(w pork.ResponseWriter, r *http.Request) {
+    code := r.FormValue("code")
+    if code == "" {
+      http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+      return
+    }
+
+    tx := oauth.Transport{
+      Config: configFromRequest(ctx, r),
+    }
+
+    _, err := tx.Exchange(code)
+    if err != nil {
+      http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+      return
+    }
+
+    var user ghUser
+    if err := fetchUser(&tx, &user); err != nil {
+      http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+      return
+    }
+
+    fmt.Fprintf(w, "%v\n", user)
   })
 }
